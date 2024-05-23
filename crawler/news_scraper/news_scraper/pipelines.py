8a991c425a2e0663d17a7de073d5c7d8b6b001b8
LoukasPap <loukas.ppd@gmail.com>
2024-05-23 18:01:41 +0300

    Update scrapper's MongoDB pipeline to insert initial recommendation score for articles

--- crawler/news_scraper/news_scraper/pipelines.py
+++ crawler/news_scraper/news_scraper/pipelines.py
@@ -1,9 +1,10 @@
 from itemadapter import ItemAdapter
-import re
 from datetime import datetime
 from scrapy.exceptions import DropItem
 import pymongo
-
+from math import e
+from typing import Final
+import re
 
 class DuplicatesPipeline:
     def __init__(self):
@@ -19,8 +20,11 @@ class DuplicatesPipeline:
 
 
 class MongoDBPipeline:
-    articles_collection = "articles"
-    authors_collection = "authors"
+    articles_collection: str = "articles"
+    articles_scores_collection: str = "articles"
+    authors_collection: str = "authors"
+
+    DECAY_CONSTANT: Final[float] = 0.1
 
     def __init__(self, mongo_uri, mongo_db):
         self.mongo_uri = mongo_uri
@@ -43,6 +47,8 @@ class MongoDBPipeline:
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
 
+        adapter["datetime"] = adapter["datetime"].replace(microsecond=0)
+
         self.db[self.articles_collection].update_one(
             {
                 "_id": adapter["url"]
@@ -55,6 +61,23 @@ class MongoDBPipeline:
                     "datetime": adapter["datetime"],
                     "new_site": adapter["news_site"],
                     "category": adapter["category"]
+                }
+            },
+            upsert=True
+        )
+        
+        recency_score = e ** (-self.DECAY_CONSTANT * round((datetime.now().replace(microsecond=0) - adapter["datetime"]).total_seconds() / 3600) )
+
+        self.db[self.articles_scores_collection].update_one(
+            {
+                "_id": adapter["url"]
+            },
+            {
+                "$set": {
+                    "trend_score": 0, # T score = 0.4 * R + 0.6 * V
+                    "views": 0, # V score = views / Vmax, where Vmax are the most likes in a 2weeks period
+                    "recency_score": recency_score, # R score = e^(-λ*R), where R=hours passed
+
                 }
             },
             upsert=True
