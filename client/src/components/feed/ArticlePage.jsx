1525066f41703b47284f2b3fdfa9f90cc889d367
LoukasPap <loukas.ppd@gmail.com>
2024-05-30 00:56:47 +0300

    Add logger for user's view activity

--- client/src/components/feed/ArticlePage.jsx
+++ client/src/components/feed/ArticlePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useLocation, Link as RRLink, useNavigate } from "react-router-dom";
+import { useLocation, Link as RRLink, useNavigate, redirect } from "react-router-dom";
 import {
   Box,
   Flex,
@@ -10,62 +10,146 @@ import {
   Avatar,
   HStack,
   Link,
+  IconButton,
+  Button
 } from "@chakra-ui/react";
 import BranchSVG from "../svg_components/BranchSVG";
+import DOMPurify from "dompurify";
 
 const ArticlePage = () => {
+  const [username, setUsername] = useState([]);
+  const [articleBody, setArticleBody] = useState("");
+
   const { state } = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
-    const getFeed = () => {
-      if (state === null) {
-        console.log("NUll");
-        navigate("/home", { state: { error: "Bad article ID" } });
+    if (state === null) {
+      console.log(state);
+      navigate("/home", { state: { error: "Bad article ID" } });
+    }
+    console.log("Good article ID");
+    setArticleBody(
+      DOMPurify.sanitize(
+        state.data.body.replaceAll(
+          "<h2>",
+          `<br><br><h2 style="font-size:1.5em; font-weight:300; color:#C5C909;">`
+        )
+      )
+    );
+  }, []);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const request = await fetch(`${window.apiIP}/user`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      const response = await request.json();
+
+      if (response && response.data) {
+        setUsername(response.data.username);
+      } else {
+        console.log("Bad token!");
+        navigate("/", { replace: true });
       }
-      console.log("testesd");
     };
 
-    getFeed();
+    getUser();
   }, []);
 
   return (
     <Box>
       {state && state.data ? "" : ""}
 
-      <Flex justifyContent={"center"}>
+      <Flex justifyContent="center">
         <VStack
-          alignItems={"start"}
+          alignItems="start"
           // border={"2px black solid"}
           w="75%"
           pl="5"
           pr="5"
           h="100%"
         >
-          <Box visibility={"visible"} bg="green" w="100%" h={"100px"}>
-            early bird
-          </Box>
-          <Box>hello</Box>
+          <Flex
+            justifyContent="space-between"
+            alignItems="center"
+            zIndex="1"
+            w="100%"
+            h="100px"
+            bg="#F5FFE4"
+            position="sticky"
+            top="0"
+            pt="4"
+            borderBottom="2px solid #FF3131"
+            // backdropFilter="blur(5px)"
+          >
+            <RRLink to={`/home`}>
+              <Heading
+                as={"h1"}
+                fontWeight={"400"}
+                fontStyle={"normal"}
+                fontSize={"3em"}
+                color="#FF3131"
+                alignSelf="end"
+              >
+                early bird
+              </Heading>
+              <Text mb="5px" fontFamily="Modern No. 20" fontSize="2xl">
+                a daily standard
+              </Text>
+            </RRLink>
 
-          <Box w="100%" mb="5" bg="#1F1F1F" borderRadius="30px" p="20">
+            <HStack>
+              <Text fontSize="xl">
+                {username && username != " " ? username : "-:-"}
+              </Text>
+              <Avatar
+                name="Dan Abrahmov"
+                size="lg"
+                src="https://bit.ly/code-beast"
+              />
+            </HStack>
+          </Flex>
+
+          <Box
+            w="100%"
+            bg="#1F1F1F"
+            borderBottomEndRadius="30px"
+            borderBottomStartRadius="30px"
+            p="20"
+          >
             <Flex
               // border="2px solid blue"
               justifyContent="space-between"
               alignItems="center"
               mb="2"
-              
             >
-              <BranchSVG></BranchSVG>
+              {/* <Button
+                // isRound={true}
+                bg="transparent"
+                // border="1px"
+                // borderColor="red.800"
+                // color="white"
+                _hover={{ color: "white"}}
+                leftIcon={<BranchSVG/>}
+                >
+                  Branch it
+              </Button> */}
+                <BranchSVG></BranchSVG>
+
               <Text
                 color="#A7A7A7"
                 textAlign="center"
                 fontSize="xl"
-                fontWeight="200">
-                {state && state.data ?
-                `${state.data.datetime} ⬝ ${state.data.new_site} ⬝ ${state.data.category}` :
-                 ""}
+                fontWeight="200"
+              >
+                {state && state.data
+                  ? `${state.data.datetime} ⬝ ${state.data.new_site} ⬝ ${state.data.category}`
+                  : ""}
               </Text>
-            
+
               <Link
                 color="#C5C909"
                 fontSize="xl"
@@ -73,7 +157,7 @@ const ArticlePage = () => {
                 href={state && state.data ? state.data._id : ""}
                 isExternal={true}
               >
-                source
+                <u>source</u>
               </Link>
             </Flex>
 
@@ -86,26 +170,27 @@ const ArticlePage = () => {
               {state && state.data ? state.data.title : ""}
             </Heading>
 
-            <Stack direction="row" mt="5" justifyContent={"center"}>
-            {state && state.data ? state.data.authors.map((author) => (
-                <HStack>
-                  <Avatar name={author} size="sm" />
-                  <Text color={"#A7A7A7"} fontWeight="200" fontSize={"xl"}>
-                    {author}
-                  </Text>
-                </HStack>
-              )) : ""}
-            </Stack>
+            <HStack direction="row" mt="5" justifyContent="center">
+              {state && state.data
+                ? state.data.authors.map((author) => (
+                    <>
+                      <Avatar name={author} size="sm" zIndex="-0" />
+                      <Text color="#A7A7A7" fontWeight="200" fontSize="xl">
+                        {author}
+                      </Text>
+                    </>
+                  ))
+                : ""}
+            </HStack>
 
-            <Text color="#FFF" mt="5" fontSize="23px" fontWeight="200">
-
-            {state && state.data ? state.data.body : ""}
+            <Text color="#FFF" mt="5" fontSize="2xl" fontWeight="200">
+              {/* {state && state.data ? state.data.body : ""} */}
+              <div dangerouslySetInnerHTML={{ __html: articleBody }} />
             </Text>
           </Box>
 
-
-          <Text>
-            {/* Recomends here */}
+          <Text textAlign="center" m="5" w="100%">
+            ©2024 Early Bird Greece, Inc. All rights reserved.
           </Text>
         </VStack>
       </Flex>
--- client/src/components/feed/ArticlesOrder.jsx
+++ client/src/components/feed/ArticlesOrder.jsx
@@ -1,36 +1,78 @@
 import React, { useState, useEffect } from "react";
+
 import {
-  Box,
-  Divider,
   VStack,
   HStack,
-  Heading,
-  Text,
-  AvatarGroup,
-  Avatar,
-  Link,
 } from "@chakra-ui/react";
 
 import { Link as RRLink, useNavigate } from "react-router-dom";
 import ArticleBox from "./ArticleBox";
 
 const ArticlesOrder = ({ data }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      const token = localStorage.getItem("token");
+
+      try {
+        const response = await fetch(`${window.apiIP}/verify-token/${token}`);
+
+        if (response.ok) {
+          console.log(response.message)
+          
+        }
+      } catch (error) {
+        navigate("/home", { replace: true });
+      }
+    };
+
+    verifyToken();
+  }, []);
+
+  const updateViewHistory = async (event, articleID, articleCategory) => {
+    try {
+      console.log("updating " + articleID)
+
+      fetch(`${window.apiIP}/update_views`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({"aid": articleID, "category": articleCategory})
+      })
+      .then( (res) => {
+        console.log("Updated user read history", res);
+      })
+      .catch( (err) => {
+        throw new Error(err);
+      });
+
+      // const responseData = await response.json();
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
   return (
     <HStack align={"start"}>
-      {data.map((articlesArray, outeIndex) => (
+      {data.map((articlesArray, outerIndex) => (
         <VStack p="1">
           {articlesArray.map((articleObj, inIndex) => (
             <RRLink
               to={`/article/${articleObj.title.replace("/","-")}`}
               state={{ data: articleObj }}
+              onClick={(e) => updateViewHistory(e, articleObj._id, articleObj.category) }
             >
               <ArticleBox
                 articleData={articleObj}
-                articleIndex={`${outeIndex}_${inIndex}`}
+                articleIndex={`${outerIndex}_${inIndex}`}
               />
             </RRLink>
           ))}
         </VStack>
+        
       ))}
     </HStack>
   );
