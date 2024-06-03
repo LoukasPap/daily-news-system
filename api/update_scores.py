bcd467b4ff4c39675678627e12c07ee0631d5e83
LoukasPap <loukas.ppd@gmail.com>
2024-06-03 15:09:11 +0300

    Remove updating trend_score when executing script for recency_score. Also, add script to update likes_score.

--- api/update_scores.py
+++ api/update_scores.py
@@ -16,8 +16,8 @@ settings = db["settings"]
 users = db["users"]
 
 DECAY_CONSTANT = -0.1
-RECENCY_WEIGHT = 0.7
-VIEWS_WEIGHT = 0.3
+RECENCY_WEIGHT = 0.6
+VIEWS_WEIGHT = 0.4
 SCORING_HOURS = 5 * 24
 
 def parse_json(data):
@@ -59,92 +59,133 @@ def update_views_score():
 
 def update_recency_score():
     pipeline = [
-    {
-        '$addFields': {
-            'dateDifference': {
-                '$dateDiff': {
-                    'startDate': '$datetime', 
-                    'endDate': '$$NOW', 
-                    'unit': 'hour', 
-                    'timezone': '+07', 
-                    'startOfWeek': 'mon'
+        {
+            '$addFields': {
+                'dateDifference': {
+                    '$abs': {
+                        '$dateDiff': {
+                            'startDate': '$datetime', 
+                            'endDate': '$$NOW', 
+                            'unit': 'hour', 
+                            'timezone': '+07', 
+                            'startOfWeek': 'mon'
+                        }
+                    }
                 }
             }
-        }
-    }, {
-        '$match': {
-            'dateDifference': {
-                '$lte': SCORING_HOURS
+        }, {
+            '$match': {
+                'dateDifference': {
+                    '$lte': 120
+                }
             }
-        }
-    }, {
-        '$lookup': {
-            'from': 'settings', 
-            'as': 'vmax', 
-            'localField': '1', 
-            'foreignField': '1'
-        }
-    }, {
-        '$addFields': {
-            'vmax': {
-                '$arrayElemAt': [
-                    '$vmax.value', 0
-                ]
+        }, {
+            '$lookup': {
+                'from': 'settings', 
+                'as': 'vmax', 
+                'localField': '1', 
+                'foreignField': '1'
             }
-        }
-    }, {
-        '$addFields': {
-            'recency_score': {
-                '$exp': {
-                    '$multiply': [
-                        {
-                            '$dateDiff': {
-                                'startDate': '$datetime', 
-                                'endDate': '$$NOW', 
-                                'unit': 'hour', 
-                                'timezone': '+07', 
-                                'startOfWeek': 'mon'
-                            }
-                        }, DECAY_CONSTANT
+        }, {
+            '$addFields': {
+                'vmax': {
+                    '$arrayElemAt': [
+                        '$vmax.value', 0
                     ]
                 }
             }
-        }
-    }, {
-        '$addFields': {
-            'trend_score': {
-                '$sum': [
-                    {
-                        '$multiply': [
-                            '$recency_score', RECENCY_WEIGHT
-                        ]
-                    }, {
-                        '$multiply': [
-                            '$views_score', VIEWS_WEIGHT
-                        ]
-                    }
-                ]
+        }, {
+            '$addFields': {
+                'recency_score': {
+                    '$sum': [
+                        {
+                            '$exp': {
+                                '$multiply': [
+                                    {
+                                        '$abs': {
+                                            '$dateDiff': {
+                                                'startDate': '$datetime', 
+                                                'endDate': '$$NOW', 
+                                                'unit': 'hour', 
+                                                'timezone': '+07', 
+                                                'startOfWeek': 'mon'
+                                            }
+                                        }
+                                    }, -0.1
+                                ]
+                            }
+                        }
+                    ]
+                }
+            }
+        }, {
+            '$project': {
+                'vmax': 0, 
+                'dateDifference': 0
+            }
+        }, {
+            '$merge': {
+                'into': 'articles_scores', 
+                'on': '_id', 
+                'whenMatched': 'merge', 
+                'whenNotMatched': 'discard'
             }
         }
-    }, {
-        '$project': {
-            'vmax': 0, 
-            'dateDifference': 0
-        }
-    }, {
-        '$merge': {
-            'into': 'articles_scores', 
-            'on': '_id', 
-            'whenMatched': 'merge', 
-            'whenNotMatched': 'discard'
-        }
-    }
-]
+    ]
     
     cursor = articles_scores.aggregate(pipeline)
     parsed_cursos = parse_json(cursor)
     print("yes")
     print(parsed_cursos)
+
+
+def update_likes_score():
+    pipeline = [
+        {
+            '$lookup': {
+                'from': 'settings', 
+                'as': 'max_likes', 
+                'pipeline': [
+                    {
+                        '$match': {
+                            '_id': 'max_likes'
+                        }
+                    }
+                ]
+            }
+        }, {
+            '$addFields': {
+                'max_likes': {
+                    '$first': '$max_likes.value'
+                }
+            }
+        }, {
+            '$addFields': {
+                'likes_score': {
+                    '$divide': [
+                        '$likes', '$max_likes'
+                    ]
+                }
+            }
+        }, {
+            '$project': {
+                'max_likes': 0
+            }
+        }, {
+            '$merge': {
+                'into': 'articles_scores', 
+                'on': '_id', 
+                'whenMatched': 'merge', 
+                'whenNotMatched': 'discard'
+            }
+        }
+    ]
+
+    cursor = articles_scores.aggregate(pipeline)
+    parsed_cursos = parse_json(cursor)
+    print("yes")
+    print(parsed_cursos)
+
 
 
 def update_all_scores():
@@ -226,4 +267,5 @@ def update_all_scores():
 # uncomment and execute
 # update_views_score()
 # update_recency_score()
+update_likes_score()
 
