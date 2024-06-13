b13fa2a9d5e4056a64d29b75ef97408267c4c493
LoukasPap <loukas.ppd@gmail.com>
2024-06-13 16:21:30 +0300

    Delete unused components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,21 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
-import ProtectedPage from "./components/Protected";
 import Home from "./components/Home";
 import ArticlePage from "./components/feed/ArticlePage";
-import { Box, Button, Spinner } from "@chakra-ui/react";
 
-// import AuthProvider from "./components/AuthProvider";
-
-// import Routes from "./routes/index";
 
 const App = () => {
   const [apiData, setApiData] = useState([]);
-  const [token, setToken] = useState([]);
 
   window.apiIP = "http://localhost:8000";
 
   return (
-    // <AuthProvider>
-    //   <Routes />
-    // </AuthProvider>
 
     <Router>
       <Routes>
         <Route path="*" element={<div>Not Found</div>} />
         <Route path="/" element={<Login />} />
-        <Route path="/protected" element={<ProtectedPage />} />
         <Route
           path="/home"
           element={<Home onDataFetch={setApiData} userData={apiData} />}
